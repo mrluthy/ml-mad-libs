fa49d87f031ef2127eb7bd14bfeaeffe6e323239
Michael Luthy <you@example.com>
2018-08-20 15:57:01 -0600

    two way binding

--- src/components/card.js
+++ src/components/card.js
@@ -3,9 +3,28 @@ import React, { Component } from 'react';
 import Input from './input';
 
 class Card extends Component {
+
+    constructor () {
+        super ()
+
+        this.state = {
+            color: 'BLUE',
+            pluralNoun: '',
+        }
+
+        this.handleInputChange = this.handleInputChange.bind(this);
+    }
+
+        handleInputChange (event) {
+            
+            this.setState({ color: event.target.value})
+        }
+// Two way binding
     render () {
         return (
             <div className='card'>
+                <h1>{this.state.color}</h1>
+                <input value={this.state.color} onChange = {(event) => this.handleInputChange(event)}/>
                 { Input ('Color')}
                 { Input ('Plural Noun')}
             </div>
