b97e93fae631fe36274273717e94641e48ddae83
Michael Luthy <you@example.com>
2018-08-22 09:05:31 -0600

    More styles

--- src/components/input.js
+++ src/components/input.js
@@ -1,8 +1,9 @@
 import React from 'react';
 // not essential named arguments
-const Input = ({title, state, name}, onChange) => {
+const Input = ({title, state, name}, onChange, index) => {
     return (
-        <div key={name} className= "input">
+        <div key={index} className= "input">
+            <label>{index}</label>
            <input name={name} value = {state} onChange={onChange}/>
            <label>{title}</label>
            
